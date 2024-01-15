a57872817faa003e15a51a2e14111719cffb2d1b
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-16 02:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let s="#";
+	for(let i=1;i<shortHex.length;i++){
+		s=s+shortHex[i]+shortHex[i];
+	}
+	return s;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex)); 
